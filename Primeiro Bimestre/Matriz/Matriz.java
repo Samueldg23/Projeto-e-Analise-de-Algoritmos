7eb17582ceda3231823b466b338c604496c25b82
Samuel Duarte <samueldgomes00@gmail.com>
2025-09-16 21:22:46 -0300

    ajuste na formatação e comentários do código de ordenação

--- Primeiro Bimestre/Matriz/Matriz.java
+++ Primeiro Bimestre/Matriz/Matriz.java
@@ -1,16 +1,18 @@
 public class Matriz {
     public static void main(String[] args) {
         int[][] matriz = {
-            {9, 2, 7},
-            {4, 6, 1},
-            {8, 3, 5}
+                { 9, 2, 7 },
+                { 4, 6, 1 },
+                { 8, 3, 5 }
         };
-        // uma espécie de bubble sort
-        for (int i = 0; i < matriz.length; i++) {    
-            for (int j = 0; j < matriz[i].length - 1; j++) { 
+        //bubble
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length - 1; j++) {
+                //criando uma nova matriz maior para comparar com j
                 for (int k = j + 1; k < matriz[i].length; k++) {
                     if (matriz[i][j] > matriz[i][k]) {
                         int temp = matriz[i][j];
+                        //troca de valores
                         matriz[i][j] = matriz[i][k];
                         matriz[i][k] = temp;
                     }
@@ -24,5 +26,6 @@ public class Matriz {
             }
             System.out.println();
         }
+        System.out.println("------------------");  
     }
 }
